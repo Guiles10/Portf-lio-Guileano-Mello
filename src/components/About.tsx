52bcd3ef5b8446d9015a7c67c988cadab87451a7
Guiles <guiles.mello@gmail.com>
2026-04-21 16:36:14 -0300

    docs: finalize about me text with full cycle vision

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -20,10 +20,10 @@ export default function About() {
                 lineHeight: "1.8"
               }}
             >
-              Atuo como desenvolvedor Full Stack (Frontend e Backend) focado em construir soluções digitais e SaaS de alta performance. 
-              Especialista em ecossistemas modernos com JavaScript, TypeScript, Node.js, React, Next.js e Nest.js, 
-              trabalho desde a arquitetura técnica com APIs REST e banco de dados até a implementação de aplicações otimizadas para conversão, 
-              garantindo eficiência, velocidade e uma experiência sólida ao usuário.
+              Sou formado como desenvolvedor Web Full Stack, com uma visão Full Cycle, levando em consideração não apenas o código e os dados mas também o impacto e a adaptabilidade do sistema no ambiente real do usuário. 
+              Focado em construir soluções digitais de alta performance utilizando diversas tecnologias e ecossistemas como JavaScript, TypeScript, Node.js, React, Next.js e Nest.js. 
+              Já construí projetos (Sites, E-commerce, Sistemas SaaS) completos linkando Frontend, Backend, banco de dados, APIs REST... até levar para ambiente de produção. 
+              Garantindo eficiência, velocidade e uma experiência sólida ao usuário.
             </p>
             <p
               style={{
