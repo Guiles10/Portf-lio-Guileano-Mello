1f3d6a16b867e5be95599c3b8369a5bd6639d31b
Guiles <guiles.mello@gmail.com>
2026-04-22 09:09:09 -0300

    style: restore previous title in About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -38,7 +38,7 @@ export default function About() {
 
           <div className="about-content">
             <h2 className="section-title" style={{ marginBottom: "2rem" }}>
-              Perfil dinâmico, desenvolvedor fullcycle
+              Resultados Orientados por Códigos & Dados
             </h2>
             <p
               style={{
